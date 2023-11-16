2b19d2791773e0e25bdcbdc1d8b5611be05212a9
Yosra_Mekaoui <yosra.mekaoui@esprit.tn>
2023-11-16 01:50:27 +0100

    CrossOrigin

--- src/main/java/tn/esprit/devops_project/controllers/ActivitySectorController.java
+++ src/main/java/tn/esprit/devops_project/controllers/ActivitySectorController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ActivitySectorController {
 
     IActivitySector activitySectorService;
